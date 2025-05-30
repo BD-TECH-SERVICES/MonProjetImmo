c0bb20cfeb50f2b46371b932c7f0e527cc21dce3
jouan-bdtechservices <corentin.jouan@bdtechservices.fr>
2025-05-30 14:55:19 +0200

    Secure dashboards and add static pages

--- main/tests/test_views.py
+++ main/tests/test_views.py
@@ -10,3 +10,17 @@ class ViewTests(TestCase):
         response = self.client.get(reverse('login'))
         self.assertEqual(response.status_code, 200)
 
+    def test_static_pages_get(self):
+        self.assertEqual(self.client.get(reverse('nos_missions')).status_code, 200)
+        self.assertEqual(self.client.get(reverse('credit')).status_code, 200)
+
+    def test_protected_views_redirect(self):
+        response = self.client.get(reverse('parcours'))
+        self.assertEqual(response.status_code, 302)
+        self.assertIn(reverse('login'), response.url)
+
+        response = self.client.get(reverse('profession'))
+        self.assertEqual(response.status_code, 302)
+        self.assertIn(reverse('login'), response.url)
+
+
